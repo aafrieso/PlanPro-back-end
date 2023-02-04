90982242ff77587e126a018c4e0cfb5990c62f93
Megan Smith <megans766@gmail.com>
2023-02-04 13:34:28 -0500

    fix show controller bug

--- controllers/task.js
+++ controllers/task.js
@@ -33,7 +33,7 @@ const show = async (req, res) => {
   try {
     const task = await Task.findById(req.params.id)
       .populate('owner')
-      .populate('task.owner')
+      .populate('note.owner')
     res.status(200).json(task)
   } catch (error) {
     res.status(500).json(error)
