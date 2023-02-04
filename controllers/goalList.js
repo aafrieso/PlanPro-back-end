a16e14024fd6266cf6e3ec2e94cb16a2fa543dbf
Austin Friesorger <aafrieso@svsu.edu>
2023-02-04 16:39:26 -0500

    update goalList functions

--- controllers/goalList.js
+++ controllers/goalList.js
@@ -1,64 +1,63 @@
 import { goalList } from "../models/goalList";
 
 const createGoal = async (req, res) => {
-  for (let key in req.body) {
-    if (req.body[key] ==='') delete req.body[key]
-  }
-  goalList.create(req.body)
-  .then(goal => {
-    res.status(201).json(goal)
-  })
-  .catch(err => {
+  try {
+    req.body.owner = req.user.profile
+    const goalList = await goalList.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { goalLists: goalList }},
+      { new: true }
+    )
+    goalList.owner = profile
+    res.status(201).json(goalList)
+  } catch(err) {
     console.log(err)
     res.status(500).json(err)
-  })
+    }
+  }
+
+const goalIndex = async (req, res) => {
+  try {
+    const goalList = await goalList.find({})
+      .populate('owner')
+      sort({ createdAt: 'desc' })
+    res.status(200).json(goalList)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 }
 
 const showGoal = async (req, res) => {
-  goalList.findbyId(req.params.id)
-  .then(event => {
-    res.json(goalList)
-  })
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
+  try {
+    const goalList = await goalList.findById(req.params.id).populate("owner")
+    res.status(200).json(goalList)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 }
 
-const goalIndex = async (req, res) => {
-  goalList.find({})
-  .then(goalList => {
-    res.json(goalList)
-  })
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
+const updateGoal = async (req, res) => {
+  try {
+    const goalList = await goalList.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).populate('owner')
+    res.status(200).json(goalList)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 }
 
 const deleteGoal = async (req, res) => {
-  goalList.findByIdAndDelete(req.params.id)
-  .then(deletedGoalList => {
-    res.json(deletedGoalList)
-  })
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
-}
-
-const updateGoal = async (req,res) => {
-  for (let key in req.body) {
-    if (req.body[key] === '') delete req.body[key]
+  try {
+    const goalList = await goalList.findByIdAndDelete(req.params.id)
+    const profile = await Profile.findById(req.user.profile)
+    profile.tenants.remove({ _id: req.params.id })
+    await profile.save()
+    res.status(200).json(tenant)
+  } catch (err) {
+    res.status(500).json(err)
   }
-  goalList.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  .then(updatedGoalList => {
-    res.json(updatedGoalList)
-  })
-  .catch(err => {
-    console.log(err)
-    res.json(err)
-  })
 }
 
 export {
