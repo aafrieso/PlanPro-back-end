782f4b2c5a2e89d78bceeb62e92e8fe7dc70a9a8
Megan Smith <megans766@gmail.com>
2023-02-06 10:04:06 -0500

    update create task and goalList

--- controllers/task.js
+++ controllers/task.js
@@ -1,17 +1,17 @@
 import { Profile } from "../models/profile.js"
 import { Task } from "../models/task.js"
+import { GoalList } from "../models/goalList.js"
 
 const create = async (req, res) => {
   try {
     req.body.owner = req.user.profile
     const task = await Task.create(req.body)
-    const profile = await Profile.findByIdAndUpdate(
-      req.user.profile,
+    const goalList = await GoalList.findByIdAndUpdate(
+      req.body.goalListId,
       { $push: { tasks: task } },
       { new: true }
-    )
-    task.owner = profile
-    res.status(201).json(task)
+    ).populate('tasks')
+    res.status(201).json(goalList)
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
