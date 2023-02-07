8804f26f90b871f61b7220ee99253b3f89c1901e
Megan Smith <megans766@gmail.com>
2023-02-06 23:40:09 -0500

    update file names

--- routes/category.js
+++ routes/category.js
@@ -2,11 +2,9 @@ import { Router } from "express"
 import * as categoryCtrl from "../controllers/category.js"
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
-
 const router = Router ()
 
 router.use(decodeUserFromToken)
 router.post('/task/:id/categories', checkAuth, categoryCtrl.createCategory) 
-
 
 export { router }
